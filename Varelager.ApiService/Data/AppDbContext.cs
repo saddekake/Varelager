23af00ebf4537ef4396a81d9cb737ca34736f412
Saddekake <miurhojax@gmail.com>
2026-03-13 13:34:20 +0100

    db structure created (product, customer and supplier tables) with their respective columns

--- Varelager.ApiService/Data/AppDbContext.cs
+++ Varelager.ApiService/Data/AppDbContext.cs
@@ -15,4 +15,32 @@ namespace Varelager.ApiService.Data
         public int Id { get; set; }
         public string Name { get; set; } = "";
     }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int SupplierId { get; set; } // "<foreign key name>Id" efcore naming convention
+        public Supplier Supplier { get; set; } = null!; // foreign key
+        public double Weight { get; set; }
+        public int Stock { get; set; }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FName { get; set; } = "";
+        public string LName { get; set; } = "";
+        public int Phone { get; set; }
+        public string Email { get; set; } = "";
+        public string Country { get; set; } = "";
+    }
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Phone { get; set; }
+        public string Email { get; set; } = "";
+        public string Country { get; set; } = "";
+    }
 }
